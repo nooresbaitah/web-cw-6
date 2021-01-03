7a594e654cc7760a01084ebfd574debb8c4e230f
nooresbaitah <76534456+nooresbaitah@users.noreply.github.com>
2021-01-03 21:40:18 +0300

    classwork6

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,15 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+function sayHello(name){
+   
+    if (name==null)  {
+        
+        console.log('يا إلهي')
+  }
+  else{
+    console.log(`Hello ${name}`);
+  
+  }
+}
+
+sayHello(`Noor`); // argument 
+sayHello(); // calling empty not expecting an argument
